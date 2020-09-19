7401bea551bba4c9119720a76ab525f3f5d8abfc
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-09-19 13:25:50 +0200

    Add build folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.arguments(express.static(__dirname + '/dist/angular10-dynamic-reactive-forms'));
+app.use(express.static(__dirname + '/dist/angular10-dynamic-reactive-forms'));
 app.get('/*', (res, req) => {
     res.sendFile(path.join(__dirname + '/dist/angular10-dynamic-reactive-forms/index.html'));
 })
